a56241881189ada129f1c0512e823b382f67ae1e
Stefan <108880675+miysono@users.noreply.github.com>
2023-08-17 19:16:35 +0300

    changes

--- script.js
+++ script.js
@@ -50,3 +50,68 @@ const init = function () {
 };
 
 init();
+
+//get answers to your questions animation
+
+//add transitionduration -> remove after when added css
+document.querySelectorAll(`.questions-photo`).forEach((photo) => {
+  photo.style.transitionDuration = "0.5s";
+});
+
+const answersContainer = document.querySelector(`.question-section`);
+answersContainer.addEventListener(`mouseover`, function (e) {
+  const clicked = e.target.closest(`.questions-photo`);
+  if (!clicked) return;
+  document.querySelectorAll(`.questions-photo`).forEach((photo) => {
+    photo.classList.remove(`col-lg-6`);
+    photo.classList.add(`col-lg-5`);
+  });
+  clicked.classList.add(`col-lg-6`);
+  clicked.classList.remove(`col-lg-5`);
+});
+
+//second answers container
+
+//add transitionduration -> remove after when added css
+document.querySelectorAll(`.second-questions-photo`).forEach((photo) => {
+  photo.style.transitionDuration = "0.5s";
+});
+
+//redo this -> i don`t like
+const secondAnswersContainer = document.querySelector(
+  `.second-question-section`
+);
+secondAnswersContainer.addEventListener(`mouseover`, function (e) {
+  const clicked = e.target.closest(`.second-questions-photo`);
+  if (!clicked) return;
+  document.querySelectorAll(`.second-questions-photo`).forEach((photo) => {
+    photo.classList.remove(`col-lg-4`);
+    photo.classList.remove(`col-lg-3`);
+    photo.classList.remove(`col-lg-2`);
+  });
+  switch (clicked.dataset.id) {
+    case "0": {
+      document
+        .querySelectorAll(`.second-questions-photo`)
+        .forEach((photo, index) => {
+          photo.classList.add(`col-lg-${4 - index}`);
+        });
+      break;
+    }
+    case "1": {
+      const photo = document.querySelectorAll(`.second-questions-photo`);
+      photo[0].classList.add(`col-lg-2`);
+      photo[1].classList.add(`col-lg-4`);
+      photo[2].classList.add(`col-lg-2`);
+      break;
+    }
+    case "2": {
+      document
+        .querySelectorAll(`.second-questions-photo`)
+        .forEach((photo, index) => {
+          photo.classList.add(`col-lg-${index + 2}`);
+        });
+      break;
+    }
+  }
+});
